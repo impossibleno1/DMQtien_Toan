ee81fc017d447a96ca687ac135e9ac891e9deb7b
phuoctoan <impossibleno1@gmail.com>
2018-11-21 10:44:24 +0700

    empty project

---
+++ QTIENDEMO002/QTIENDEMO002/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="QTIENDEMO002.MvcApplication" Language="C#" %>
---
+++ QTIENDEMO002/QTIENDEMO002/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(QTIENDEMO002.Startup))]
+namespace QTIENDEMO002
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
